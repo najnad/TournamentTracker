4f16d10c2b0f578822c7187986165cefd31df0cc
najnad <n4jn4d@gmail.com>
2021-05-28 18:44:56 -0700

    Added a constructor + overloaded constructor that parses through user input

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -7,6 +7,11 @@ namespace TrackerLibrary
     public class PrizeModel
     {
         /// <summary>
+        /// Primary key for the prize.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
         /// Represents a place number for the prize.
         /// </summary>
         public int PlaceNumber { get; set; }
@@ -28,6 +33,29 @@ namespace TrackerLibrary
         /// Represents the prize percentage.
         /// </summary>
         public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+
+        }
 
     }
 }
