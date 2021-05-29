ff8f85b02bc5878590a0644381c867a1ec9da284
najnad <n4jn4d@gmail.com>
2021-05-28 18:39:23 -0700

    Created GlobalConfig to initialize connections to respective databases

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+
+            if (textFiles)
+            {
+                TextConnector text = new TextConnector();
+                Connections.Add(text);
+            }
+        }
+    }
+}
