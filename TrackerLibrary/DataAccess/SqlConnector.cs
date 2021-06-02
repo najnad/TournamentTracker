0e9937d98eb7d425a942d9a7d8790d168bb74688
najnad <n4jn4d@gmail.com>
2021-06-02 16:02:17 -0700

    Added sql and text connectors; added create prize function in sql

---
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
+{
+    public class SqlConnector : IDataConnection
+    {
+        // TODO - Connect SQL database.
+        /// <summary>
+        /// Saves a prize to the database.
+        /// </summary>
+        /// <param name="model">A prize object</param>
+        /// <returns>A prize object, inlcuding its properties.</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
+
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
+{
+    public class TextConnector : IDataConnection
+    {
+        // TODO - Connect text file database.
+        /// <summary>
+        /// Saves a prize to the text file database.
+        /// </summary>
+        /// <param name="model">A prize object</param>
+        /// <returns>A prize object, inlcuding its properties.</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
