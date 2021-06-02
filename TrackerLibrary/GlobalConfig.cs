016de094b14db6250f08ee85f88f49bc75b6bafd
najnad <n4jn4d@gmail.com>
2021-06-02 16:01:08 -0700

    Modified database function to either be sql or text; not both.

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,31 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static IDataConnection Connection { get; private set; }
 
         /// <summary>
         /// Initializes sql database and/or text file database.
         /// </summary>
         /// <param name="database">bool: true if sql database exists.</param>
         /// <param name="textFiles">bool: true if text file database exists.</param>
-        public static void InitializeConnections(bool database, bool textFiles)
-        {
-            if (database)
+        public static void InitializeConnection(DatabaseType db)
+        { 
+            if (db == DatabaseType.Sql)
             {
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
-
-            if (textFiles)
+            else  if (db == DatabaseType.TextFile)
             {
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
