80b60644b79d491f03a5ce7fe72f9bc1f3746a94
najnad <n4jn4d@gmail.com>
2021-06-03 00:05:16 -0700

    Added function to save prizes into text database.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
-        // TODO - Connect text file database.
+        private const string PrizesFile = "PrizeModels.csv";
+
         /// <summary>
         /// Saves a prize to the text file database.
         /// </summary>
@@ -15,7 +18,26 @@ namespace TrackerLibrary.DataAccess
         /// <returns>A prize object, inlcuding its properties.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load text file
+            // Convert text to List<PrizeMOdel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with the new ID
+            prizes.Add(model);
+
+            // Convert the prizes to list<String>
+            // Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
