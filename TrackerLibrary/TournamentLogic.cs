e676592bd72448181a5532aef0bb3f70d4125cef
najnad <n4jn4d@gmail.com>
2021-06-12 17:43:18 -0700

    Added methods for email sending, Added CompleteTournament methods

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -26,6 +26,7 @@ namespace TrackerLibrary
 
         public static void UpdateTournamentResults(TournamentModel model)
         {
+            int startingRound = model.CheckCurrentRound();
             List<MatchupModel> toScore = new List<MatchupModel>();
             foreach (List<MatchupModel> round in model.Rounds)
             {
@@ -43,6 +44,195 @@ namespace TrackerLibrary
             AdvanceWinners(toScore, model);
 
             toScore.ForEach(x => GlobalConfig.Connection.UpdateMatchup(x));
+
+            int endingRound = model.CheckCurrentRound();
+
+            if (endingRound > startingRound)
+            {
+                // Send email to users.
+                // model.AlertUsersToNewRound();
+            }
+        }
+
+        /// <summary>
+        /// Method that triggers email deliveries.
+        /// </summary>
+        /// <param name="model">TournamentModel</param>
+        public static void AlertUsersToNewRound(this TournamentModel model)
+        {
+            int currentRound = model.CheckCurrentRound();
+            List<MatchupModel> currentMatchupsInRound = model.Rounds.Where(x => x.First().MatchupRound == currentRound).First();
+
+            foreach (MatchupModel matchup in currentMatchupsInRound)
+            {
+                foreach (MatchupEntryModel me in matchup.Entries)
+                {
+                    foreach (PersonModel p in me.TeamCompeting.TeamMembers)
+                    {
+                        AlertPlayerToNewRound(p, me.TeamCompeting.TeamName, matchup.Entries.Where(x => x.TeamCompeting != me.TeamCompeting).FirstOrDefault());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Helper method to AlertUsersToNewRound().
+        /// Constructs email that is sent to players.
+        /// </summary>
+        /// <param name="player">PersonModel</param>
+        /// <param name="teamName">string</param>
+        /// <param name="competitor">MatchupEntryModel</param>
+        private static void AlertPlayerToNewRound(PersonModel player, string teamName, MatchupEntryModel competitor)
+        {
+            if (player.EmailAddress.Length == 0)
+            {
+                return;
+            }
+
+            string to = "";
+            string subject = "";
+            StringBuilder body = new StringBuilder();
+
+            if (competitor != null)
+            {
+                subject = $"Next match against { competitor.TeamCompeting.TeamName }: READY to be played.";
+
+                body.AppendLine("<h1>New match ready!</h1>");
+                body.Append("<strong>Next match against: </strong>");
+                body.Append(competitor.TeamCompeting.TeamName);
+                body.AppendLine();
+                body.AppendLine();
+                body.AppendLine("<h3>Good luck!</h3>");
+            }
+            else
+            {
+                subject = "Congratulations, your team was selected for a BYE round.";
+
+                body.AppendLine("You will be notified when your next match is ready!");
+            }
+
+            to = player.EmailAddress;
+
+            // EmailLogic.SendEmail(to, subject, body.ToString());
+        }
+
+        /// <summary>
+        /// Checks current round of the tournament.
+        /// </summary>
+        /// <param name="model">TournamentModel</param>
+        /// <returns>int; current round</returns>
+        private static int CheckCurrentRound(this TournamentModel model)
+        {
+            int output = 1;
+
+            foreach (List<MatchupModel> round in model.Rounds)
+            {
+                if (round.All(x => x.Winner != null)) 
+                {
+                    output += 1;
+                }
+                else
+                {
+                    return output;
+                }
+            }
+
+            // Tournament Over
+            CompleteTournament(model);
+
+            return output - 1;
+        }
+
+        /// <summary>
+        /// Calculates prizes, constructs winning email, triggers CompleteTournament event.
+        /// </summary>
+        /// <param name="model">TournamentModel</param>
+        private static void CompleteTournament(TournamentModel model)
+        {
+            GlobalConfig.Connection.CompleteTournament(model);
+
+            TeamModel winner = model.Rounds.Last().First().Winner;
+            TeamModel runnerUp = model.Rounds.Last().First().Entries.Where(x => x.TeamCompeting != winner).First().TeamCompeting;
+
+            decimal winnerPrize = 0;
+            decimal runnerUpPrize = 0;
+            if (model.Prizes.Count > 0)
+            {
+                decimal prizePool = model.EnteredTeams.Count * model.EntryFee;
+                PrizeModel firstPlacePrize = model.Prizes.Where(x => x.PlaceNumber == 1).FirstOrDefault();
+                PrizeModel secondPlacePrize = model.Prizes.Where(x => x.PlaceNumber == 2).FirstOrDefault();
+               
+                if (firstPlacePrize != null)
+                {
+                    winnerPrize = firstPlacePrize.CalculatePrize(prizePool);
+                }
+
+                if (secondPlacePrize != null)
+                {
+                    runnerUpPrize = secondPlacePrize.CalculatePrize(prizePool);
+                }
+            }
+
+            // Send championsip email
+            string subject = "";
+            StringBuilder body = new StringBuilder();
+
+            subject = $"{ model.TournamentName } Winner: { winner.TeamName }";
+
+            body.AppendLine("<h1>Congratulations to the winning team!</h1>");
+            body.AppendLine("<p>Thank you to everyone that participated in this tournament.</p>");
+            body.AppendLine("<br />");
+
+            if (winnerPrize > 0)
+            {
+                body.AppendLine($"<p>{ winner.TeamName } wins ${ winnerPrize }!</p>");
+            }
+
+            if (runnerUpPrize > 0)
+            {
+                body.AppendLine($"<p>{ runnerUp.TeamName } wins ${ runnerUpPrize }!</p>");
+            }
+
+            body.AppendLine();
+            body.AppendLine("<p>See you in the next one!</p>");
+
+            List<string> bcc = new List<string>();
+
+            foreach (TeamModel t in model.EnteredTeams) {
+                foreach (PersonModel p in t.TeamMembers)
+                {
+                    if (p.EmailAddress.Length > 0)
+                    {
+                        bcc.Add(p.EmailAddress); 
+                    }
+                }
+            }
+
+            // EmailLogic.SendEmail(new List<string>(), bcc, subject, body.ToString());
+
+            model.CompleteTournament();
+        }
+
+        /// <summary>
+        /// Calculates prizes in the tournament.
+        /// </summary>
+        /// <param name="p">PrizeModel</param>
+        /// <param name="prizePool">decimal</param>
+        /// <returns>decimal; prize amount</returns>
+        private static decimal CalculatePrize(this PrizeModel p, decimal prizePool)
+        {
+            decimal output = 0;
+
+            if (p.PrizeAmount > 0)
+            {
+                output = p.PrizeAmount;
+            }
+            else
+            {
+                output = Decimal.Multiply(prizePool, Convert.ToDecimal(p.PrizePercentage / 100));
+            }
+
+            return output;
         }
 
         private static void AdvanceWinners(List<MatchupModel> models, TournamentModel tournament)
